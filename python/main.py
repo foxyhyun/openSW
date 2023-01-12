dc097cfa09aa6564b6463ad688b3c0955feb8da4
YeonbiJung <79094754+YeonbiJung@users.noreply.github.com>
2023-01-12 13:23:57 +0900

    그래프 대충 완성

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 from Stock import Stock
-from predict import *
-import matplotlib.pyplot as plt
+from predict import predictNextDay
+from plotGraph import plotGraph
+
 
 coid = input("회사명 : ")
 days = int(input("일 수"))
@@ -10,11 +11,13 @@ CO = Stock(coid)
 prices, date = CO.returnPrices()
 idx = CO.returnIdx()
 
-prediction = []
+prices=prices[-1148:,:]
+dates = date[-1148:,0].tolist()
 
-for i in range(5):
+for i in range(days):
     predictedPrice = predictNextDay(prices, idx)[0,0]
     prices = np.concatenate((prices, np.array([predictedPrice,prices[-1,1]]).reshape(1,-1)), axis=0)
-    prediction.append(predictedPrice)
+    dates.append("%s일 후"%(i+1))
+    
+plotGraph(prices, dates)
 
-print(prediction)
---
+++ python/plotGraph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plotGraph(prices, dates):
+    data_df = pd.DataFrame(prices[:,0], dates)
+    
+    plt.figure(figsize=(15,10))
+    plt.plot(data_df.index[0:1148], data_df[0:1148], color='blue')
+    plt.plot(data_df.index[1147:], data_df[1147:], color='red')
+    plt.show()
